a07ee7053666fca2684c37feb70505b509bff440
BoRuei1028 <andy6o43222@gmail.com>
2022-12-29 11:05:26 +0800

    Revert "feat: add route"
    This reverts commit a5d8c64908004e114c558a94ddda8a301b169923.

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 
-const Reurl = require('./models/reurl')
-
 if (process.env.NODE_ENV !== "production") {
   require('dotenv').config()
 }
 
 const app = express()
-
-const port = 3000
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -21,28 +17,14 @@ db.once('open', () => {
   console.log('mongodb connect')
 })
 
+const port = 3000
 
-
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true })) //body-parser => post
 
 
 app.get('/', (req, res) => {
   res.render('index')
-})
-
-app.post('/shortener', (req, res) => {
-  const inputUrl = req.body.url
-  return Reurl.create({ inputUrl })
-    .then(() => res.redirect('result'))
-    .catch((error) => console.log(error))
-})
-
-app.get('/result', (req, res) => {
-  // 這裡我需要將5碼亂數以及正確的url準備好
-  res.render('result')
 })
 
 app.listen(port, () => {
