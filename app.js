d68b2811e253ba7ef114f564828a60a9110c8923
BoRuei1028 <andy6o43222@gmail.com>
2023-01-02 16:10:02 +0800

    refactor: 重構mongoose引入

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 
 const Reurl = require('./models/reurl')
-const getRandomString = require('./getRandomString')
-
-if (process.env.NODE_ENV !== "production") {
-  require('dotenv').config()
-}
+const getRandomString = require('./public/javascripts/getRandomString')
+require('./config/mongoose')
 
 const app = express()
 
 const port = 3000
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error')
-})
-db.once('open', () => {
-  console.log('mongodb connect')
-})
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true })) //body-parser => post
+app.use(express.urlencoded({ extended: true })) 
 
 
 app.get('/', (req, res) => {
@@ -39,7 +26,7 @@ app.post('/', (req, res) => {
     .lean()
     .then((item) => {
       //找不到 => item === null => create => render
-      if (!item) { 
+      if (!item) {
         item = { inputUrl, randomString }
         return Reurl.create(item)
           .then(() => res.render('result', { item }))
